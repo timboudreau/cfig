e7597d38532ac5e09328594ebfdb0dbb40ee3936
Tim Boudreau <niftiness@gmail.com>
2014-01-07 14:55:45 -0500

    Initial content

--- README.md
+++ README.md
@@ -1,4 +1,80 @@
-cfig
-====
+cfig - Configuration Manager
+============================
 
-Yet another NodeJS JSON configuration loading library
+Yet another NodeJS JSON configuration loading + command-line argument parsing 
+library.  Because the world needed another and I've written this code too many
+times not to have it in a library.
+
+The purpose is to provide a standard, straightforward way of reading configuration
+from configuration files and the command line, and encapsulating that information.
+
+Usage
+-----
+
+In a file called ``myapp.js``:
+
+        var Configuration = require( 'cfig' );
+
+        var defaults = { foo : 'bar', baz : 42 }
+
+        new Configuration( defaults, function(err, cfig) {
+
+            // cfig now contains the defaults, overridden by values from
+            // /etc/myapp.json, ~/myapp.json and ./myapp.json
+            // overridden again by any command-line arguments in the form
+            // --key value
+        });
+
+Constructor arguments to ``Configuration`` can be supplied in any order;  there
+can be any, none or all of:
+
+ * An array of strings - directories to look in for JSON files, instead of ``/etc/``, ``~/`` and ``./``
+ * An object (non-array javascript hash) containing default values
+ * A string - the name for those JSON files
+ * A boolean - if true, log the files it's reading as it reads them
+ * A function - to call back once all files have been read
+
+What It Does
+------------
+
+ * Loads configuration from JSON files and gives you back a Javascript object of them
+ * By default, looks for ``$NAME.json`` in ``/etc``, ``~/`` and ``./`` - that is
+    * System-wide configuration in ``/etc``
+    * Per-user configuration in the current user home (if any)
+    * Per-session configuration in the process working dir
+    * Overrides any of those with command-line arguments
+ * The file name is the file name of the file that launched the process, minus any ``.js``
+    * Unless you pass a string argument to the Configuration constructor
+ * Iterates all command-line arguments (``process.argv.slice(2)``) and processes them,
+assuming a format ``--key value`` for long-form arguments.  ``Configuration.addExpansions()``
+allows you to provide a hash of short (e.g. ``-k``) arguments and what long key names they
+should map to.
+
+
+Command-Line Arguments
+----------------------
+
+The process arguments are also taken into account.  Say you want to allow short-form
+unix style arguments, which can be combined - i.e. ``-xvf`` is the same as ``-x -v -f``
+is the same as ``--extract --verbose --file``:
+
+        var expansions = {
+            x: 'extract',
+            v: 'verbose',
+            f: 'file'
+        }
+        var Configuration = require( 'cfig' ).addExpansions( expansions );
+
+        var defaults = { verbose : false }
+
+        new Configuration(defaults, function(err, cfig) {
+            
+        });
+
+If you pass ``-xvf`` you get
+
+    {
+        extract : true,
+        verbose : true,
+        file : true
+    }
---
+++ cfig.js
@@ -0,0 +1,201 @@
+var path = require('path'), hoek = require('hoek'), fs = require('fs'),
+        util = require('util'), cmdline = require('./cmdline');
+
+function reverse(arr) {
+    var result = [];
+    for (var i = arr.length - 1; i >= 0; i--) {
+        result.push(arr[i]);
+    }
+    return result;
+}
+
+function getUserHome() {
+    return process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'] || '~/';
+}
+
+function Configuration() {
+    var self = this;
+    var dirs = null;
+    var name = null;
+    var defaults = null;
+    var callback = null;
+    var log = false;
+    for (var i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] === 'undefined') {
+            continue;
+        } else if (util.isArray(arguments[i])) {
+            if (dirs) {
+                throw new Error("More than one array argument passed");
+            }
+            dirs = arguments[i];
+        } else if (typeof arguments[i] === 'string') {
+            if (name) {
+                throw new Error("More than one string argument passed");
+            }
+            name = arguments[i];
+        } else if (typeof arguments[i] === 'object' && !util.isArray(arguments[i])) {
+            if (defaults) {
+                throw new Error("More than one object argument passed");
+            }
+            defaults = arguments[i];
+        } else if (typeof arguments[i] === 'function') {
+            if (callback) {
+                throw new Error("More than one function argument passed");
+            }
+            callback = arguments[i];
+        } else if (typeof arguments[i] === 'boolean') {
+            if (log) {
+                throw new Error("More than one boolean argument passed")
+            }
+            log = arguments[i];
+        }
+    }
+    dirs = dirs || ['/etc', getUserHome(), './'];
+    defaults = defaults || {};
+    if (!name) {
+        name = path.basename(require.main.filename, '.js');
+    }
+    var fn = name + '.json';
+
+    function readOne(dir, base, cb) {
+        var fl = path.join(dir, fn);
+        if (log) {
+            console.log('Read settings from ' + fl);
+        }
+        fs.exists(fl, function(exists) {
+            if (exists) {
+                fs.readFile(fl, 'utf8', function(err, data) {
+                    if (err)
+                        return cb(err);
+                    try {
+                        var obj = JSON.parse(data);
+                        var res = hoek.applyToDefaults(base, obj);
+                        cb(null, res);
+                    } catch (err) {
+                        cb(err, data);
+                    }
+                });
+            } else {
+//                NOT TRYING OTHER DIRECTORIES IF ONE FAILS!!
+                cb(null, base);
+            }
+        });
+    }
+
+    var arr = reverse(dirs);
+    function go(err, data) {
+        if (err) {
+            if (callback) {
+                return callback(err);
+            } else
+                throw err;
+        }
+        if (arr.length === 0) {
+            for (var key in data) {
+                self[key] = data[key];
+            }
+            var last = cmdline.parseArgs(Configuration.expansions);
+            for (var key in last) {
+                self[key] = last[key];
+            }
+            if (callback) {
+                callback(null, self);
+            }
+        } else {
+            var d = arr.pop();
+            readOne(d, data, go);
+        }
+    }
+
+    function reload(cb) {
+        callback = cb;
+        go(null, hoek.clone(defaults));
+    }
+    this.reload = reload;
+    reload(callback)
+}
+
+module.exports = Configuration;
+
+
+Configuration.addExpansions = function(exps) {
+    if (typeof exps !== 'object') {
+        throw new Error("Not an object: " + util.inspect(exps))
+    }
+    if (Configuration.expansions) {
+        Configuration.expansions = hoek.merge(exps, Configuration.expansions);
+    } else {
+        Configuration.expansions = exps;
+    }
+    return Configuration;
+}
+
+//test
+if (require.main === module) {
+    try {
+        var now = '' + new Date().getTime();
+        var tmp = require('os').tmpdir();
+
+        var dir1 = path.join(tmp, 'configTest_' + now + '_1');
+        var dir2 = path.join(tmp, 'configTest_' + now + '_2');
+        var dir3 = path.join(tmp, 'configTest_' + now + '_3');
+
+        fs.mkdirSync(dir1)
+        fs.mkdirSync(dir2)
+        fs.mkdirSync(dir3)
+
+        fs.writeFileSync(path.join(dir1, 'config.json'), JSON.stringify({foo: 'whee', fromOne: true}))
+        fs.writeFileSync(path.join(dir2, 'config.json'), JSON.stringify({foo: 'moo', fromTwo: true}))
+        fs.writeFileSync(path.join(dir3, 'config.json'), JSON.stringify({foo: 'whatzit', fromThree: true, wug: null}))
+
+        var assert = require('assert')
+
+        var ds = {foo: 'bar', baz: 23, wug: 'moo'}
+        var wasRun = false;
+        new Configuration(true, ds, function(err, dta) {
+            assert.ifError(err);
+            assert.equal(dta.foo, 'whatzit');
+            assert.equal(dta.baz, 23);
+            assert.equal(dta.fromOne, true)
+            assert.equal(dta.fromTwo, true)
+            assert.equal(dta.fromThree, true)
+            assert.equal(dta.wug, 'moo');
+            wasRun = true;
+        }, [dir1, dir2, dir3]);
+
+        setTimeout(function() {
+            assert.equal(true, wasRun)
+            wasRun = false;
+            Configuration.addExpansions({f: 'foo'})
+            Configuration.addExpansions({q: 'quux'})
+            assert.deepEqual(Configuration.expansions, {f: 'foo', q: 'quux'})
+            new Configuration.addExpansions({b: 'brr'})(true, function(err, cfig) {
+                assert.deepEqual(Configuration.expansions, {f: 'foo', q: 'quux', b: 'brr'})
+                wasRun = true;
+            });
+            setTimeout(function() {
+                assert.equal(wasRun, true, "Code was not run");
+
+                process.argv = ['node', 'foo', '-fp', '--mub', 'bar', 'quux', '-b', 'monkey', 'woob'];
+                new Configuration(true, { foo : 35 }, function(err, cfig) {
+                    console.log('HEY!!')
+                    console.log(util.inspect(cfig));
+                    delete cfig.reload;
+                    assert.deepEqual(cfig, {foo: true,
+                        p: true,
+                        mub: 'bar',
+                        _last: 'woob',
+                        quux: true,
+                        brr: 'monkey',
+                        woob: true});
+                    console.log('Done.');
+                });
+
+            }, 100)
+        }, 100)
+
+    } catch (err) {
+        console.log(err)
+        process.exit(1)
+    }
+}
