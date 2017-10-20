1f0cff997075a8b9d47898392b8b9ba73aaaf4d0
Tim Boudreau <tim@timboudreau.com>
2017-10-20 02:40:37 -0400

    Move tests to separate script; no semantic changes

--- cfig.js
+++ cfig.js
@@ -1,35 +1,27 @@
 /*
-The MIT License (MIT)
-
-Copyright (c) 2014 Tim Boudreau
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ The MIT License (MIT)
+ 
+ Copyright (c) 2014 Tim Boudreau
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-var path = require('path'), hoek = require('hoek'), fs = require('fs'),
-        util = require('util'), cmdline = require('./cmdline');
-
-function reverse(arr) {
-    var result = [];
-    for (var i = arr.length - 1; i >= 0; i--) {
-        result.push(arr[i]);
-    }
-    return result;
-}
+var path = require( 'path' ), hoek = require( 'hoek' ), fs = require( 'fs' ),
+        util = require( 'util' ), cmdline = require( './cmdline' );
 
 function getUserHome() {
     return process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'] || '~/';
@@ -45,92 +37,101 @@ function Configuration() {
     for (var i = 0; i < arguments.length; i++) {
         if (typeof arguments[i] === 'undefined') {
             continue;
-        } else if (util.isArray(arguments[i])) {
+        } else if (util.isArray( arguments[i] )) {
             if (dirs) {
-                throw new Error("More than one array argument passed");
+                throw new Error( "More than one array argument passed" );
             }
             dirs = arguments[i];
         } else if (typeof arguments[i] === 'string') {
             if (name) {
-                throw new Error("More than one string argument passed");
+                throw new Error( "More than one string argument passed" );
             }
             name = arguments[i];
-        } else if (typeof arguments[i] === 'object' && !util.isArray(arguments[i])) {
+        } else if (typeof arguments[i] === 'object' && !util.isArray( arguments[i] )) {
             if (defaults) {
-                throw new Error("More than one object argument passed");
+                throw new Error( "More than one object argument passed" );
             }
             defaults = arguments[i];
         } else if (typeof arguments[i] === 'function') {
             if (callback) {
-                throw new Error("More than one function argument passed");
+                throw new Error( "More than one function argument passed" );
             }
             callback = arguments[i];
         } else if (typeof arguments[i] === 'boolean') {
             if (log) {
-                throw new Error("More than one boolean argument passed")
+                throw new Error( "More than one boolean argument passed" );
             }
             log = arguments[i];
         }
     }
-    dirs = dirs || ['/etc', '/opt/local/etc', getUserHome(), './', '/etc/cfig'];
+    dirs = dirs || [ '/etc', '/opt/local/etc', getUserHome(), './', '/etc/cfig' ];
     defaults = defaults || {};
     if (!name) {
-        name = path.basename(require.main.filename, '.js');
+        name = path.basename( require.main.filename, '.js' );
     }
     var fn = name + '.json';
 
-    function httpFetch(url, base, cb) {
-        var client = require('./minimal-http-client');
+    function httpFetch( url, base, cb ) {
+        var client = require( './minimal-http-client' );
         if (log) {
-            console.log('Download settings from ' + url);
+            console.log( 'Download settings from ' + url );
         }
-        client(url, headers[url], function (err, data) {
+        client( url, headers[url], function ( err, data ) {
             if (err) {
-                return cb(err);
+                return cb( err );
             }
             try {
-                data = JSON.parse(data);
-            } catch (err2) {
-                return cb(err2);
+                data = JSON.parse( data );
+                if (log) {
+                    console.log( 'Downloaded ' + url, data );
+                }
+            } catch ( err2 ) {
+                return cb( err2 );
             }
-            var res = hoek.applyToDefaults(base, data);
-            cb(null, res);
-        });
+            console.log( 'APPLY ', data );
+            var res = hoek.applyToDefaults( base, data );
+            cb( null, res );
+        } );
     }
 
     var urlTest = /^http[s]?:\/\/.*?/;
-    function readOne(dir, base, cb) {
-        if (urlTest.test(dir)) {
-            return httpFetch(dir, base, cb);
+    function readOne( dir, base, cb ) {
+        if (urlTest.test( dir )) {
+            return httpFetch( dir, base, cb );
         }
-        var fl = path.join(dir, fn);
+        var fl = path.join( dir, fn );
         if (log) {
-            console.log('Read settings from ' + fl);
+            console.log( 'Read settings from ' + fl );
         }
-        fs.exists(fl, function (exists) {
+        fs.exists( fl, function ( exists ) {
             if (exists) {
-                fs.readFile(fl, 'utf8', function (err, data) {
+                fs.readFile( fl, 'utf8', function ( err, data ) {
                     if (err)
-                        return cb(err);
+                        return cb( err );
                     try {
-                        var obj = JSON.parse(data);
-                        var res = hoek.applyToDefaults(base, obj);
-                        cb(null, res);
-                    } catch (err) {
-                        cb(err, data);
+                        var obj = JSON.parse( data );
+                        if (log) {
+                            console.log( fl, obj );
+                        }
+                        var res = hoek.applyToDefaults( base, obj );
+                        cb( null, res );
+                    } catch ( err ) {
+                        console.error( 'Bad json in ' + fl, err.message );
+                        cb( err, data );
                     }
-                });
+                } );
             } else {
-                cb(null, base);
+                console.log( '...no such file ' + fl );
+                cb( null, base );
             }
-        });
+        } );
     }
 
-    var arr = reverse(dirs);
-    function go(err, data) {
+    var arr = hoek.clone( dirs ).reverse();
+    function go( err, data ) {
         if (err) {
             if (callback) {
-                return callback(err);
+                return callback( err );
             } else
                 throw err;
         }
@@ -138,33 +139,33 @@ function Configuration() {
             for (var key in data) {
                 self[key] = data[key];
             }
-            var last = cmdline.parseArgs(Configuration.expansions, Configuration.args || process.argv.slice(2));
-            hoek.merge(self, last);
+            var last = cmdline.parseArgs( Configuration.expansions, Configuration.args || process.argv.slice( 2 ) );
+            hoek.merge( self, last );
             if (callback) {
-                callback(null, self);
+                callback( null, self );
             }
         } else {
             var d = arr.pop();
-            readOne(d, data, go);
+            readOne( d, data, go );
         }
     }
 
-    function reload(cb) {
+    function reload( cb ) {
         callback = cb;
-        go(null, hoek.clone(defaults));
+        go( null, hoek.clone( defaults ) );
     }
     this.reload = reload;
-    reload(callback)
+    reload( callback );
 }
 
 module.exports = Configuration;
 
-Configuration.addExpansions = function (exps) {
+Configuration.addExpansions = function ( exps ) {
     if (typeof exps !== 'object') {
-        throw new Error("Not an object: " + util.inspect(exps))
+        throw new Error( "Not an object: " + util.inspect( exps ) )
     }
     if (Configuration.expansions) {
-        Configuration.expansions = hoek.merge(exps, Configuration.expansions);
+        Configuration.expansions = hoek.merge( exps, Configuration.expansions );
     } else {
         Configuration.expansions = exps;
     }
@@ -173,108 +174,6 @@ Configuration.addExpansions = function (exps) {
 
 var headers = {};
 
-Configuration.addHeaders = function (url, hdrs) {
+Configuration.addHeaders = function ( url, hdrs ) {
     headers[url] = hdrs;
-}
-
-//test
-if (require.main === module) {
-    try {
-        var now = '' + new Date().getTime();
-        var tmp = require('os').tmpdir();
-
-        var dir1 = path.join(tmp, 'configTest_' + now + '_1');
-        var dir2 = path.join(tmp, 'configTest_' + now + '_2');
-        var dir3 = path.join(tmp, 'configTest_' + now + '_3');
-
-        fs.mkdirSync(dir1)
-        fs.mkdirSync(dir2)
-        fs.mkdirSync(dir3)
-
-        fs.writeFileSync(path.join(dir1, 'cfig.json'), JSON.stringify({foo: 'whee', fromOne: true}))
-        fs.writeFileSync(path.join(dir2, 'cfig.json'), JSON.stringify({foo: 'moo', fromTwo: true}))
-        fs.writeFileSync(path.join(dir3, 'cfig.json'), JSON.stringify({foo: 'whatzit', fromThree: true, wug: null}))
-
-        var assert = require('assert')
-
-        new Configuration(true, ['http://timboudreau.com/files/scottenfinn.json'], function (err, dta) {
-            console.log('URL LOAD GOT ' + util.inspect(dta));
-        });
-
-        var ds = {foo: 'bar', baz: 23, wug: 'moo'}
-        var wasRun = false;
-        new Configuration(true, ds, function (err, dta) {
-            assert.ifError(err);
-            assert.equal(dta.baz, 23);
-            assert.equal(dta.foo, 'whatzit', "Should be whatzit is " + dta.foo);
-            assert.equal(dta.fromOne, true)
-            assert.equal(dta.fromTwo, true)
-            assert.equal(dta.fromThree, true)
-            assert.equal(dta.wug, 'moo');
-            wasRun = true;
-        }, [dir1, dir2, dir3]);
-
-        setTimeout(function () {
-            assert.equal(true, wasRun)
-            wasRun = false;
-            Configuration.addExpansions({f: 'foo'})
-            Configuration.addExpansions({q: 'quux'})
-            assert.deepEqual(Configuration.expansions, {f: 'foo', q: 'quux'})
-            new Configuration.addExpansions({b: 'brr'})(true, function (err, cfig) {
-                if (err)
-                    throw err;
-                assert.deepEqual(Configuration.expansions, {f: 'foo', q: 'quux', b: 'brr'})
-                wasRun = true;
-            });
-            setTimeout(function () {
-                assert.equal(wasRun, true, "Code was not run");
-
-                process.argv = ['node', 'foo', '-fp', '--mub', 'bar', 'quux', '-b', 'monkey', 'woob', '--hey.you', '23'];
-                new Configuration(true, {foo: 35}, function (err, cfig) {
-                    if (err)
-                        throw err;
-                    console.log(util.inspect(cfig));
-                    delete cfig.reload;
-                    assert.deepEqual(cfig, {foo: true,
-                        p: true,
-                        mub: 'bar',
-                        quux: true,
-                        brr: 'monkey',
-                        hey: {you: 23},
-                        woob: true});
-
-                    const defaults = {
-                        queue: {
-                            prefix: 'bq',
-                            stallInterval: 15000,
-                            redis: {
-                                host: '127.0.0.1',
-                                port: 6379,
-                                db: 0,
-                                options: {}
-                            },
-                            getEvents: false,
-                            sendEvents: true,
-                            isWorker: false,
-                            removeOnSuccess: true,
-                            catchExceptions: false
-                        },
-                        test: false
-                    };
-                    Configuration.args = ['--queue.redis.port', '3207', '-r', 'redis.foo.com'];
-                    Configuration.addExpansions({r: 'queue.redis.host'});
-                    new Configuration(defaults, [], function (err, info) {
-                        if (err)
-                            throw err;
-                        console.log(util.inspect(info, {depth: 10}));
-                        assert.deepEqual(info.queue.redis, {host: 'redis.foo.com', port: 3207, db: 0, options: {}});
-                    });
-                    console.log('Done.');
-                });
-            }, 100)
-        }, 100);
-    } catch (err) {
-        console.log(err)
-        process.exit(1)
-    }
 }
---
+++ test.js
@@ -0,0 +1,253 @@
+#!/usr/bin/env node
+const Configuration = require( './cfig' )
+        , parseArgs = require( './cmdline' ).parseArgs
+        , fs = require( 'fs' )
+        , assert = require( 'assert' )
+        , path = require( 'path' )
+        , util = require( 'util' )
+        ;
+
+const cleanupDirs = [ ];
+const cleanupFiles = [ ];
+function cleanup() {
+    cleanupFiles.forEach( ( file ) => {
+        fs.unlinkSync( file );
+    } );
+    cleanupDirs.forEach( ( dir ) => {
+        fs.rmdirSync( dir );
+    } );
+}
+
+function mkdir( dir ) {
+    fs.mkdirSync( dir );
+    cleanupDirs.push( dir );
+}
+
+function writeFile( file, data ) {
+    if (typeof data !== 'string') {
+        data = JSON.stringify( data );
+    }
+    fs.writeFileSync( file, data, {encoding: 'utf8'} );
+    cleanupFiles.push( file );
+}
+
+function start( onstart, reqs ) {
+    const http = require( 'http' );
+    var server = http.createServer( ( req, res ) => {
+        res.writeHead( 200, {
+            'Content-Type': 'text/plain; charset=UTF-8'
+        } );
+        reqs.push( req );
+        res.end( JSON.stringify( {http: true, thing: 'whatever', whee: 23, boo: 54} ) );
+    } ).listen( 7638, "127.0.0.1", () => {
+        onstart();
+    } );
+    return ( cb ) => {
+        server.close( cb );
+    };
+}
+
+function testHttp() {
+    const reqs = [ ];
+    var stop;
+    var sdata = null;
+    stop = start( () => {
+        new Configuration( [ 'http://127.0.0.1:7638' ], {boo: 58, wug : 40}, function ( err, dta ) {
+            if (err) {
+                throw err;
+            }
+            sdata = dta;
+            assert( reqs.length > 0 );
+            assert( sdata );
+            assert( sdata.http );
+            assert.equal( sdata.thing, 'whatever', 'Whatever not set' );
+            assert.equal( sdata.whee, 23, 'whee wrong value' );
+            assert.equal( sdata.boo, 54, 'boo wrong value' );
+            assert.equal( sdata.wug, 40, 'wug wrong value' );
+            stop( () => {
+            } );
+        } );
+    }, reqs );
+}
+
+const fixture1 = {foo: 'whee', fromOne: true};
+const fixture2 = {foo: 'moo', fromTwo: true};
+const fixture3 = {foo: 'whatzit', fromThree: true, wug: null};
+const now = '' + new Date().getTime();
+const tmp = require( 'os' ).tmpdir();
+const dir1 = path.join( tmp, 'configTest_' + now + '_1' );
+const dir2 = path.join( tmp, 'configTest_' + now + '_2' );
+const dir3 = path.join( tmp, 'configTest_' + now + '_3' );
+const file1 = path.join( dir1, 'test.json' );
+const file2 = path.join( dir2, 'test.json' );
+const file3 = path.join( dir3, 'test.json' );
+
+
+function setupFiles() {
+
+    mkdir( dir1 );
+    mkdir( dir2 );
+    mkdir( dir3 );
+
+    writeFile( file1, fixture1 );
+    writeFile( file2, fixture2 );
+    writeFile( file3, fixture3 );
+
+}
+
+function testCfig() {
+    setupFiles();
+    var ds = {foo: 'bar', baz: 23, wug: 'moo'};
+    var wasRun = false;
+    new Configuration( true, ds, function ( err, dta ) {
+        assert.ifError( err );
+        assert.equal( dta.baz, 23 );
+        assert.equal( dta.foo, 'whatzit', "Should be whatzit is " + dta.foo );
+        assert.equal( dta.fromOne, true );
+        assert.equal( dta.fromTwo, true );
+        assert.equal( dta.fromThree, true );
+        assert.equal( dta.wug, 'moo' );
+        wasRun = true;
+    }, [ dir1, dir2, dir3 ] );
+
+    setTimeout( function () {
+        assert.equal( true, wasRun );
+        wasRun = false;
+        Configuration.addExpansions( {f: 'foo'} );
+        Configuration.addExpansions( {q: 'quux'} );
+        assert.deepEqual( Configuration.expansions, {f: 'foo', q: 'quux'} );
+        new Configuration.addExpansions( {b: 'brr'} )( true, function ( err, cfig ) {
+            if (err)
+                throw err;
+            assert.deepEqual( Configuration.expansions, {f: 'foo', q: 'quux', b: 'brr'} );
+            wasRun = true;
+        } );
+        setTimeout( function () {
+            assert.equal( wasRun, true, "Code was not run" );
+
+            process.argv = [ 'node', 'foo', '-fp', '--mub', 'bar', 'quux', '-b', 'monkey', 'woob', '--hey.you', '23' ];
+            new Configuration( true, {foo: 35}, function ( err, cfig ) {
+                if (err)
+                    throw err;
+                delete cfig.reload;
+                assert.deepEqual( cfig, {foo: true,
+                    p: true,
+                    mub: 'bar',
+                    quux: true,
+                    brr: 'monkey',
+                    hey: {you: 23},
+                    woob: true} );
+
+                const defaults = {
+                    queue: {
+                        prefix: 'bq',
+                        stallInterval: 15000,
+                        redis: {
+                            host: '127.0.0.1',
+                            port: 6379,
+                            db: 0,
+                            options: {}
+                        },
+                        getEvents: false,
+                        sendEvents: true,
+                        isWorker: false,
+                        removeOnSuccess: true,
+                        catchExceptions: false
+                    },
+                    test: false
+                };
+                Configuration.args = [ '--queue.redis.port', '3207', '-r', 'redis.foo.com' ];
+                Configuration.addExpansions( {r: 'queue.redis.host'} );
+                new Configuration( defaults, [ ], function ( err, info ) {
+                    if (err)
+                        throw err;
+                    assert.deepEqual( info.queue.redis, {host: 'redis.foo.com', port: 3207, db: 0, options: {}} );
+                } );
+            } );
+        }, 100 );
+    }, 100 );
+}
+
+function testCmdline() {
+    var x = parseArgs();
+    assert.deepEqual( x, {} );
+    var args = [ "--foo", "monkey", "-b", "whatzit", "--poodle", "hoover" ];
+
+    var expansions = {
+        f: 'foo',
+        b: 'bugle',
+        p: 'poodle'
+    };
+
+    var x = parseArgs( expansions, args );
+
+    var expect = {
+        foo: 'monkey',
+        bugle: 'whatzit',
+        poodle: 'hoover'
+    };
+
+    assert.deepEqual( x, expect );
+
+    args = [ '-f', '-p', '--bugle', 'hoo', 'hah' ];
+    expect = {
+        foo: true,
+        bugle: 'hoo',
+        poodle: true,
+        hah: true
+    };
+    x = parseArgs( expansions, args );
+    assert.deepEqual( x, expect );
+
+    args = [ '-fpb' ];
+    expect = {
+        foo: true,
+        bugle: true,
+        poodle: true
+    };
+    x = parseArgs( expansions, args );
+    assert.deepEqual( x, expect );
+
+    args = [ '-fpbqdz' ];
+    expect = {
+        foo: true,
+        bugle: true,
+        poodle: true,
+        q: true,
+        d: true,
+        z: true
+    };
+    x = parseArgs( expansions, args );
+    assert.deepEqual( x, expect );
+
+    x = parseArgs( null, [ 'food', 'bug', 'monkey', 'food', 'stew', '--foo', 'bar' ] );
+
+    assert.deepEqual( x, {food: true,
+        bug: true,
+        monkey: true,
+        stew: true,
+        foo: 'bar'} );
+
+    x = parseArgs( null, [ 'foo', 'bar.baz', 'woo.hoo.you' ] );
+    expect = {
+        foo: true, bar: {baz: true}, woo: {hoo: {you: true}}};
+
+
+    x = parseArgs( null, [ '--skiddoo', '23', '--the.meaning', '42', '--i.think.its', 'true', '--but.that.is', 'false', '--but.that.will', 'do' ] );
+    expect = {skiddoo: 23,
+        the: {meaning: 42},
+        i: {think: {its: true}},
+        but: {that: {is: false, will: 'do'}}};
+
+    assert.deepEqual( x, expect );
+}
+
+process.on( 'exit', cleanup );
+process.on( 'uncaughtException', ( err ) => {
+    console.error( err );
+    process.exit( 1 );
+} );
+
+testCfig();
+testCmdline();
+testHttp();
